d3052aec26a242774a240c812a08edb6682fb73c
adramazan <240692@astanait.edu.kz>
2026-01-31 20:30:49 +0500

    feat: main function(version 1.0)

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,16 +1,20 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"os"
 
+	"clothes-store/internal/handlers"
+	"clothes-store/internal/models"
 	"clothes-store/pkg/db"
 
 	"github.com/joho/godotenv"
 )
 
 func main() {
+	// Load Environment Variables
 	err := godotenv.Load()
 	if err != nil {
 		log.Println("Warning: .env file not found. Relying on system env vars.")
@@ -23,13 +27,44 @@ func main() {
 
 	// Initialize Database
 	dbConn := db.InitDB(connStr)
-
-	// Close the connection when main() stops
 	defer dbConn.Close()
+	fmt.Println("Database connected succesfully")
 
+	// Initialize Models
+	userModel := &models.UserModel{DB: dbConn}
+	orderModel := &models.OrderModel{DB: dbConn}
+	productModel := &models.ProductModel{DB: dbConn}
+
+	// Initialize Handlers
+	authHandler := &handlers.AuthHandler{UserModel: userModel}
+	orderHandler := &handlers.OrderHandler{OrderModel: orderModel}
+
+	storeHandler := &handlers.StoreHandler{
+		ProductModel: productModel,
+		Logger:       log.New(os.Stdout, "[STORE]", log.LstdFlags),
+	}
+
+	// Start Background Worker
+	log.Println("Starting background email worker...")
+	handlers.StartEmailWorker()
+
+	// Define Routes
+	mux := http.NewServeMux()
+
+	// Auth Routes
+	mux.HandleFunc("/signup", authHandler.SignUp)
+	mux.HandleFunc("/login", authHandler.Login)
+
+	// Order Routes
+	mux.HandleFunc("/orders", orderHandler.CreateOrder)
+
+	// Store Routes
+	mux.HandleFunc("/products", storeHandler.GetAll)
+	mux.HandleFunc("/products/create", storeHandler.Create)
+
+	// Start Server
 	port := os.Getenv("PORT")
 
-	// Start Server (Placeholder)
 	log.Printf("Server starting on :%s...", port)
 	if err := http.ListenAndServe(":"+port, nil); err != nil {
 		log.Fatal(err)
