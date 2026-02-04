21dddbcd36da6abc9a6844466a98a7079570c956
adramazan <240692@astanait.edu.kz>
2026-02-04 17:41:33 +0500

    feat: add JWT authentication middleware

--- internal/middleware/auth_mw.go
+++ internal/middleware/auth_mw.go
@@ -1,1 +1,45 @@
 package middleware
+
+import (
+	"context"
+	"net/http"
+	"strings"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+func RequireAuth(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		authHeader := r.Header.Get("Authorization")
+		if authHeader == "" {
+			http.Error(w, "Unauthorized: No token provided", http.StatusUnauthorized)
+			return
+		}
+
+		tokenString := strings.TrimPrefix(authHeader, "Bearer ")
+
+		token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+			if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+				return nil, jwt.ErrSignatureInvalid
+			}
+			return []byte("my_secret_key"), nil
+		})
+
+		if err != nil || !token.Valid {
+			http.Error(w, "Unauthorized: Invalid Token", http.StatusUnauthorized)
+			return
+		}
+
+		if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+			userId := int(claims["user_id"].(float64))
+			role := claims["role"].(string)
+
+			ctx := context.WithValue(r.Context(), "userId", userId)
+			ctx = context.WithValue(ctx, "role", role)
+
+			next.ServeHTTP(w, r.WithContext(ctx))
+		} else {
+			http.Error(w, "Unauthorized", http.StatusUnauthorized)
+		}
+	})
+}
