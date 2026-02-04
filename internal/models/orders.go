b14e044f3bee8e7a7ab07cda6001f2a10271f8d9
yerassylkm <240975@astanait.edu.kz>
2026-02-04 15:28:35 +0500

    feat: add order history, details via JOIN, and status updates

--- internal/models/orders.go
+++ internal/models/orders.go
@@ -18,6 +18,12 @@ type OrderItemInput struct {
   Quantity  int
 }
 
+type OrderItem struct {
+  ProductName   string  `json:"product_name"`
+  Price         int     `json:"price_kzt"`
+  Quantity      int     `json:"quantity"`
+}
+
 type OrderModel struct {
   DB *sql.DB
 }
@@ -28,7 +34,6 @@ func (m *OrderModel) Create(userID int, items []OrderItemInput) (int, error) {
     return 0, err
   }
 
-  // 1. Insert Order Header
   var orderID int
   stmtOrder := `INSERT INTO orders (user_id, total_price, status, created_at) 
                 VALUES ($1, 0, 'Pending', NOW()) RETURNING id`
@@ -39,7 +44,6 @@ func (m *OrderModel) Create(userID int, items []OrderItemInput) (int, error) {
     return 0, err
   }
 
-  // 2. Insert Items into order_items
   stmtItems := `INSERT INTO order_items (order_id, product_id, quantity) VALUES ($1, $2, $3)`
 
   for _, item := range items {
@@ -51,4 +55,55 @@ func (m *OrderModel) Create(userID int, items []OrderItemInput) (int, error) {
   }
 
   return orderID, tx.Commit()
+}
+
+func (m *OrderModel) GetAllByUserID(userID int) ([]Order, error) {
+  stmt := `SELECT id, user_id, total_price, status, created_at FROM orders WHERE user_id = $1 ORDER BY created_at DESC`
+
+  rows, err := m.DB.Query(stmt, userID)
+  if err != nil {
+    return nil, err
+  }
+  defer rows.Close()
+
+  var orders []Order
+  for rows.Next() {
+    var o Order
+    err = rows.Scan(&o.ID, &o.UserID, &o.TotalPrice, &o.Status, &o.CreatedAt)
+    if err != nil {
+      return nil, err
+    }
+    orders = append(orders, o)
+  }
+  return orders, nil
+}
+
+func (m *OrderModel) GetItems(orderID int) ([]OrderItem, error) {
+  stmt := `
+    SELECT p.name, p.price_kzt, oi.quantity
+    FROM order_items oi
+    JOIN products p ON oi.product_id = p.id
+    WHERE oi.order_id = $1`
+
+    rows, err := m.DB.Query(stmt, orderID)
+    if err != nil {
+      return nil, err
+    }
+    defer rows.Close()
+
+    var items []OrderItem
+    for rows.Next() {
+      var i OrderItem
+      if err := rows.Scan(&i.ProductName, &i.Price, &i.Quantity); err != nil {
+        return nil, err
+      }
+      items = append(items, i)
+    }
+    return items, nil
+}
+
+func (m *OrderModel) UpdateStatus(orderID int, status string) error{
+  stmt := `UPDATE orders SET status = $1 WHERE id = $2`
+  _, err := m.DB.Exec(stmt, status, orderID)
+  return err
 }
